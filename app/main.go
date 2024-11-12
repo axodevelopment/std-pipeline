d83bc4f47c591c8f6dc6babc7733901ed1453e17
Michael Wilson <michaelwilson@mikew-dev.local>
2024-11-12 02:22:13 -0500

    Added some json response

--- app/main.go
+++ app/main.go
@@ -5,6 +5,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Result struct {
+	Status int    `json:"status"`
+	Body   string `json:"body"`
+}
+
 func main() {
 	fmt.Println("App Starting...")
 	defer fmt.Println("App Ending ...")
@@ -13,7 +18,14 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/", func(c *gin.Context) {
-		c.String(200, "Hello")
+
+		res := Result{
+			Status: 200,
+			Body:   "Its a body",
+		}
+
+		c.JSON(200, res)
+
 	})
 
 	router.Run(":8080")
