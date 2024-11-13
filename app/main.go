62e174fd48f828eaa22c21ae137d43c4bc0b37f2
Michael Wilson <michaelwilson@mikew-dev.local>
2024-11-12 23:32:52 -0500

    does this trigger?

--- app/main.go
+++ app/main.go
@@ -14,7 +14,7 @@ func main() {
 	fmt.Println("App Starting...")
 	defer fmt.Println("App Ending ...")
 
-	fmt.Println("This is where i trigger workflow")
+	fmt.Println("This is where i trigger workflows")
 
 	// from gin tutorial
 	router := gin.Default()
